d11447f7f836663025f13ce88d443fa1e7076bdc
Roy Storey <kiwiroy@users.noreply.github.com>
2018-06-25 17:38:53 +1200

    Update util.R

--- R/util.R
+++ R/util.R
@@ -6,7 +6,33 @@
 #' @param appname    The application name
 #' @param base_path  The base path to use for storing the symmetric and encrypted files
 #'
+#' @return list()
+#'
 #' Influential environment variables \code{USE_HERE}
+#'
+#' @examples
+#' \donotrun{
+#'   # list(symmetric_file = "/home/user/.best-shiny-app-ever/symmetric.rsa",
+#'   #      key_file       = "/home/user/.best-shiny-app-ever/secret.pem",
+#'   #      vault          = "/home/user/.best-shiny-app-ever")
+#    paths <- app_secret_paths(appname = "best-shiny-app-ever")
+#'
+#'   Sys.getenv("USE_HERE"="1")
+#'   # TRUE
+#'   file.exists("/apps/another-shiny-app/.here")
+#'   # list(symmetric_file = "/apps/another-shiny-app/.user-settings/user.symmetric.rsa",
+#'   #      key_file       = "/home/user/.best-shiny-app-ever/secret.pem",
+#'   #      vault          = "/apps/another-shiny-app/.user-settings")
+#'   paths <- app_secret_paths(appname = "another-shiny-app")
+#'
+#'   Sys.unsetenv("USE_HERE")
+#'   # TRUE
+#'   file.exists("/apps/dull-app/.here")
+#'   # list(symmetric_file = "/apps/dull-app/.dull-app/user.symmetric.rsa",
+#'   #      key_file       = "/home/user/.best-shiny-app-ever/secret.pem",
+#'   #      vault          = "/apps/dull-app/.dull-app")
+#'   paths <- app_secret_paths(appname = "dull-app", base_path = file.path(here()))
+#' }
 #'
 #' @export
 app_secret_paths <- function(appname = NULL, base_path = Sys.getenv("HOME")) {
@@ -20,7 +46,7 @@ app_secret_paths <- function(appname = NULL, base_path = Sys.getenv("HOME")) {
   
   if (Sys.getenv("USE_HERE")) {
     sym_name <- paste(Sys.getenv("USER"), "symmetric.rsa", sep = ".")
-    vault <- file.path(here(), ".user-settings") 
+    vault    <- file.path(here(), ".user-settings") 
   } else {
     sym_name <- "symmetric.rsa"
     vault    <- file.path(base_path, app_dot_dir)
